73b06bd6fa36f26dc523bd883540ab9dd4f5ec94
DanielTrue <danielokezie75@gmail.com>
2022-07-29 14:29:56 +0100

    made some changes

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,26 @@
 import { Link } from "gatsby"
 import * as React from "react"
-
+import FouroFour from "./style/fourofour.css"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
-    <Seo title="404: Not found" />
-    <div className="row">
-      <h6>Sorry</h6>
-      <p>But we couldn't find the page</p>
-      <Link to="/">Go Back Home</Link>
-    </div>
-  </Layout>
+	<FouroFour>
+	  <Layout>
+	    <Seo title="404" />
+	    <div className="row">
+	      <h6>Sorry</h6>
+	      <ul>
+		      <li>4</li>
+		      <li>0</li>
+		      <li>4</li>
+	      </ul>
+	      <p>But we couldn't find the page</p>
+	      <Link to="/">Go Back Home</Link>
+	    </div>
+	  </Layout>
+	</FouroFour>
+
 )
 
 export default NotFoundPage
